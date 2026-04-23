8dcacea175d956dc5a96e3dbfa463560fc530881
Jérémy Hébert <jeray.gaming@gmail.com>
2026-04-23 08:04:54 -0400

    Création d'une branche pour les boutons

--- Button.h
+++ Button.h
@@ -2,4 +2,3 @@
 class Button
 {
 };
-
--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, world!";
-
-    system("pause>NUL");
+    std::cout << "Hello world!";
     return 0;
 }
