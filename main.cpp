d97ceb9ba73ee7e5ee0a4333ff9c6f441b4a27a0
Jérémy Hébert <53281029+JeremyHebertQc@users.noreply.github.com>
2026-04-23 10:53:52 -0400

    Update main.cpp
    Completion du merge de Vincent-Settings

--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-#include <nlohmann/json.hpp>
-#include <SFML/graphics.hpp>
-#include <string>
-
-#include "const.h"
-#include "Settings.h"
 
 int main() {
-    Settings settings("settings.json");
-    Difficulty difficulty;
-
-    
-
-    settings.setFullScr(-10);
+    std::cout << "Hello, world!";
 
     system("pause>NUL");
     return SUCCESS;
