27f97182bfec5c73ac3908d2083a8c90259207ed
felixantoinelacroix <felixantoinelacroix@gmail.com>
2026-04-30 09:18:30 -0400

    Fait le debut du fonctionnement de la classe Game.h.

--- Game.h
+++ Game.h
@@ -1,12 +1,31 @@
 #pragma once
+#include <map>
 #include <SFML/Audio.hpp>
+
+#include "Grid.h"
 #include "Scoreboard.h"
 
-class Game {
+class Game
+{
 public:
-	void showEndScreen();
-	void savePlayerScore(Player player, GameMode mode);
+	Game();
+	~Game();
 
+	void loadMainMenu();
+
+	void playGame(GameMode mode);
+
+	void loadSettingsMenu();
+	void loadTutorialMenu();
+	void loadScoreboardMenu();
+
+	void loadEndMenu(float score, GameMode mode);
+private:
+	Menu _mainMenu;
+	Menu _settingMenu;
+	Menu _tutorialMenu;
+	Scoreboard _scoreboard;
+	Grid _grid;
 };
 
 
