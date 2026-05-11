25b761a16264ee416549cc5d98d12eaa6306136d
Jérémy Hébert <53281029+JeremyHebertQc@users.noreply.github.com>
2026-05-10 22:44:56 -0400

    Adding music and sound effect to the game.

--- Game.h
+++ Game.h
@@ -3,12 +3,21 @@
 #include "Settings.h"
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 class Game
 {
   private:
 	sf::RenderWindow _window;
 	Settings _settings;
+
+	// SoundEffect declarations
+	sf::SoundBuffer _soundEffectBuffer;
+	sf::Sound _soundEffect;
+
+	// Music declarations
+	sf::SoundBuffer _musicBuffer;
+	sf::Sound _music;
 
   public:
 	// Constructor
@@ -20,6 +29,11 @@ class Game
 	// Method
 	void play();
 	bool StartGame();
+
+	// Music management
+	void playSound(const std::string& soundPath, float volume);
+	void playMusic(const std::string& soundPath, float volume);
+	void stopMusic();
 	// void showEndScreen();
 	// void savePlayerScore(Player player, GameMode mode);
 };
